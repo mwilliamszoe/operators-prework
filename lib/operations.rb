53fc32d846c8880bbc1bc6909d028242877d400b
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-20 05:37:40 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
 if speed < 40 || speed > 60
-  "Safe"
+  true
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
-age < 2 ? "baby" : "not a baby"
+# age < 2 ? "baby" : "not a baby"
